31b4924374548577472837365a4f77e5bb66fbcf
vitaleymulikacatchup <vitaley.mulika.catchup@gmail.com>
2025-10-16 17:36:35 +0300

    Update project files

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+export default function App() {
+   return (
+     <div className="color-style-1">
+      <header className="colored"> 
+            <div>  
+                <img src='https://storage.googleapis.com/svoi-upload/442/204214/1645ca44-1cec-47b6-b52b-422f680f55ff.png' alt="Logo" title="logo"/>
+                <h1 >Запорізька міська рада</h1></div></header>
+
+      <section className="contact-page section container mt--lg-100 pb--80 bgi-no-repeat bgi-size-cover contact__bg">
+            <div className="breadcrumb breadcrumb_style2 d-flex align-items-center justify-content-between flex-wrap wow fadeInUp mx-auto " data-wow-delay=".1s" style={{maxWidth: '960px'}}>
+                <div className=""><p className="breadcrumb__link breadcrumb_active text-dark font--semi-bold fs-base lh-28 wow fadeInUp mx-auto " data-wow-delay=".1s" style={{maxWidth: '960px'}}>
+                    <a href="/" className="" >Головна</a></p></div></div></section>
+
+      <section className="two-one-container mobile-two-top twoColumnContainer shortInputStyled stylyzedTextareaShortInput blackButton alternativeBlackButton sendBtnDiv captchaBlock allowCheckbox checkBoxLabel whiteSpan textAreaWhiteSpan recaptchaElement divBr brHr h3SubdomainStructure subdomainImg1 emojiRulerEmojiWave">
+            <div className="" id="_mGzvYsP4h0yb_token_"></div></section>
+
+      <section className="hromada-info reset-margins">
+            <div id='hromada-info-map' ></div></section>
+
+      <section className="colored bottom-navigation">
+            <div className="bottom-navigation-grid">
+                <div className="item">
+                    <input type="checkbox" 
+                           className="bottom-navigation-checkbox" id="bottom-navigation-checkbox1"/> 
+                     <label htmlFor="bottom-navigation-checkbox1"> <p className='header'>Громада </p></label></div></div></section>
+
+      <footer className="main-content__section footer">       //JSX for the CSS class name of this element. (className is required in React)
+            <div id='botLinksSectionId'>  {/* Define a div with an ID */}  /* Required to select it using JS or jQuery*/  
+                <p className="" >Title</p></div></footer>
+     </div>
+   );
+ }
---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        
+      },
+      fontFamily: {
+        'custom-1': ['system-ui', 'sans-serif']
+      }
+    }
+  }
+};
